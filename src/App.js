a04b629abdbaddf1a6a6ab907b1cc15c36b51596
Anusha260 <anusha20@navgurukul.org>
2022-12-27 22:34:48 +0530

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from "react"
 import './App.css';
 
 function App() {
+  const[data,setData]=useState([])
+  const getData=async()=>{
+    const response=await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const result=await response.json();
+    setData(result)
+    console.log(data)
+  }
+  useEffect(()=>{
+    getData()
+    console.log(data)
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users data</h1>
+      <div>
+     
+      <table>
+        <tr>
+          <th>name</th>
+          <th>username</th>
+          <th>email</th>
+          <th>city</th>
+          <th>zipcode</th>
+          <th>phone number</th>
+          <th>Company nme</th>
+          
+        </tr>
+     
+      
+      {
+        data.map((item,index)=>{
+          return(
+           
+             
+                
+                <tr>
+                  {/* <td><h3>{item.id}</h3></td> */}
+                  <td><p>{item.name}</p></td>
+                  <td><p>{item.username}</p></td>
+                  <td><p>{item.email}</p></td>
+                  <td><p>{item.address.city}</p></td>
+                  <td><p>{item.address.zipcode}</p></td>
+                  <td><p>{item.phone}</p></td>
+                  <td><p>{item.company.name}</p></td>
+              </tr>
+             
+            
+          )
+        })
+      }
+     </table>
+     </div>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Parent.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react'
+import Child from './Child';
+function App() {
+const [count,setCount]=useState(100)
+const [arr,setArr]=useState([1,2,3,4,5])
+  return(
+   
+    <Child count={count} setCount={setCount} arr={arr}/>
+   
+  )
+}
+export default App; 
